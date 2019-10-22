76fc1b3aa71b5dc4feb333f3c1a272e24d1c8d55
myonnahito <56863823+myonnahito@users.noreply.github.com>
2019-10-22 20:41:23 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,6 +5,7 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
@@ -14,16 +15,5 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-@bot.command()
-async def neko(ctx):
-    await ctx.send('にゃーん')
-
-@bot.command()
-async def w(ctx):
-    await ctx.send('草生やすな')
-
-@bot.command()
-async def ww(ctx):
-    await ctx.send('ダブルの衝撃ｗ')
 
 bot.run(token)
