2cb3213aed5f378e721cfb14c2ff3fe5a0e55136
myonnahito <56863823+myonnahito@users.noreply.github.com>
2019-10-22 22:30:30 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -13,9 +13,5 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
-@bot.event
-channel = client.get_channel(633125958848610305)
-await channel.send('hello')
-
 bot.run(token)
 
