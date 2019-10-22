c1a58652c9ca6ee2202f29f35e4b7b7eece9f487
myonnahito <56863823+myonnahito@users.noreply.github.com>
2019-10-22 18:35:13 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -27,20 +27,4 @@ async def w(ctx):
 async def ww(ctx):
     await ctx.send('ダブルの衝撃ｗ')
 
-@client.event
-async def on_message(message):
-    if client.user in message.mentions: # 話しかけられたかの判定
-        reply = f'{message.author.mention} 呼んだ？' # 返信メッセージの作成
-        await message.channel.send(reply) # 返信メッセージを送信
-
-@client.event
-async def on_message(message):
-    # 「おはよう」で始まるか調べる
-    if message.content.startswith("おはよう"):
-        # 送り主がBotだった場合反応したくないので
-        if client.user != message.author:
-            # メッセージを書きます
-            m = "おはようございます" + message.author.name + "さん！"
-            # メッセージが送られてきたチャンネルへメッセージを送ります
-            await message.channel.send(m)
 bot.run(token)
