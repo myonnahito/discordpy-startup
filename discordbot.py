2b4a134b51c89e7fe7d7e6e95538a3e7288b1f58
myonnahito <56863823+myonnahito@users.noreply.github.com>
2019-10-22 21:00:28 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,10 +1,34 @@
 from discord.ext import commands
 import os
 import traceback
-
+from datetime import datetime
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
+CHANNEL_ID = 一般 #チャンネルID
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
 
+
+#投稿する日時
+dateTimeList = [
+'2019/10/22 21:00',
+'2019/10/22 21:01',
+'2019/10/22 21:02',
+'2019/10/22 21:03',
+'2019/10/22 21:04',
+'2019/10/22 21:05',
+'2019/10/22 21:06'
+]
+
+# 起動時に動作する処理
+@client.event
+async def on_ready():
+    print('ready')
+
+# 指定時間に走る処理
+async def SendMessage():
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send('時間だよ')
 
 @bot.event
 async def on_command_error(ctx, error):
@@ -17,4 +41,3 @@ async def ping(ctx):
 
 bot.run(token)
 
-client.run(token)
