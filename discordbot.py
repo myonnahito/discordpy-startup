87b56de8f701c322726bd7a6a2c1930dd9c72bed
myonnahito <56863823+myonnahito@users.noreply.github.com>
2019-10-22 21:23:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,8 @@
 from discord.ext import commands
-from datetime import datetime
 import os
 import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-CHANNEL_ID = 一般 #チャンネルID
-
-#投稿する日時
-dateTimeList = [
-'2019/10/22 21:00',
-'2019/10/22 21:01',
-'2019/10/22 21:02',
-'2019/10/22 21:03',
-'2019/10/22 21:04',
-'2019/10/22 21:05',
-'2019/10/22 21:06'
-]
 
 @bot.event
 async def on_command_error(ctx, error):
