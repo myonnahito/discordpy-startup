92cb9ad1cd0a6de455391ef0b92bf3fe864393cb
myonnahito <56863823+myonnahito@users.noreply.github.com>
2019-10-22 22:56:11 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,13 +1,18 @@
 from discord.ext import commands
 import os
 import traceback
-import danabiBot
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
+
+@bot.event
+async def on_message(message):
+    # do some extra stuff here
+
+    await bot.process_commands(message)
 
 @bot.command()
 async def ping(ctx):
