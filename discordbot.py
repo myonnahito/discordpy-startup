ceb56d2cd1790d926321f88e1055a33675386a42
myonnahito <56863823+myonnahito@users.noreply.github.com>
2019-10-22 16:58:02 +0900

    指定時間

--- discordbot.py
+++ discordbot.py
@@ -15,23 +15,68 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
+async def ねこ(ctx):
+    await ctx.send('にゃーん')
+    
+#coding:UTF-8
+import discord
+from datetime import datetime
+from discord.ext import tasks
+
+TOKEN = "**********" #トークン
+CHANNEL_ID = ********** #チャンネルID
+# 接続に必要なオブジェクトを生成
+client = discord.Client()
+
+
+#投稿する日時
+dateTimeList = [
+'2019/10/22 17:00',
+'2019/10/22 18:00',
+'2019/10/22 19:00',
+'2019/10/22 20:00',
+'2019/10/22 21:00',
+'2019/10/22 22:00',
+'2019/10/22 23:00',
+]
+
+# 起動時に動作する処理
 @client.event
 async def on_ready():
-    print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
-    print('------')
+    print('ready')
 
+# 指定時間に走る処理
+async def SendMessage():
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send('時間だよ')
+
+# 30秒に一回ループ
+@tasks.loop(seconds=30)
+async def time_check():
+    sleepTime = 0
+    # 現在の時刻
+    now = datetime.now().strftime('%Y/%m/%d %H:%M')
+    if now in dateTimeList :
+        print(now)
+        await SendMessage()
+        #該当時間だった場合は２重に投稿しないよう３０秒余計に待機
+        await asyncio.sleep(30)
+
+# メッセージ受信時に動作する処理
 @client.event
 async def on_message(message):
-    # 「おはよう」で始まるか調べる
-    if message.content.startswith("おはよう"):
-        # 送り主がBotだった場合反応したくないので
-        if client.user != message.author:
-            # メッセージを書きます
-            m = "おはようございます" + message.author.name + "さん！"
-            # メッセージが送られてきたチャンネルへメッセージを送ります
-            await message.channel.send(m)
+    # メッセージ送信者がBotだった場合は無視する
+    if message.author.bot:
+        return
+    # 使用できるコマンド一覧
+    if message.content == '!help':
+        await message.channel.send('現在使用できるコマンドはありません')
+
+#ループ処理
+time_check.start()
+# Botの起動とDiscordサーバーへの接続
+client.run(TOKEN)
 
     
     
