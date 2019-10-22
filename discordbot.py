75fdc27f121ff7041c2ab302b752b4fd497cc96c
myonnahito <56863823+myonnahito@users.noreply.github.com>
2019-10-22 20:31:40 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,9 +5,11 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
+
 
 @bot.command()
 async def ping(ctx):
