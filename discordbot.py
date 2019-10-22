5fac27a0396356a2dc321c626716335dd6e135b4
myonnahito <56863823+myonnahito@users.noreply.github.com>
2019-10-22 20:36:20 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -5,7 +5,6 @@ import traceback
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
-
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
