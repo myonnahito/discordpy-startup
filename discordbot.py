e90b0c1eb419be5bc9ca4464a2413c267c9d500c
myonnahito <56863823+myonnahito@users.noreply.github.com>
2019-10-24 01:03:41 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -7,25 +7,6 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 CHANNEL_ID = 633125958848610308 #チャンネルID
 
-#投稿する日時
-dateTimeList = [
-'2019/10/23 19:30',
-'2019/10/23 19:40',
-'2019/10/23 19:50',
-'2019/10/23 20:00',
-'2019/10/23 20:10',
-'2019/10/23 20:20',
-'2019/10/23 20:30'
-]
-
-# 60秒に一回ループ
-@tasks.loop(seconds=60)
-async def loop():
-    # 現在の時刻
-    now = datetime.now().strftime('%H:%M')
-    if now == '21:15':
-        channel = client.get_channel(CHANNEL_ID)
-        await channel.send('おはよう')  
 
 # 指定時間に走る処理
 async def SendMessage():
@@ -52,6 +33,61 @@ async def on_message(message):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
+@bot.command()
+async def タクティパンツ(ctx):
+    await ctx.send('ｱｰｯｱｯｱｯｱｯﾊｯﾊｯﾊｯﾎｯﾊｯﾎｯﾎｯﾎｯﾎｰｩﾎｰｩﾎｰｩﾎｰｩﾎｫｫｰ!!ﾌｫｰｯﾌﾌｫｰｯﾌﾌｫｰｯﾌﾌｫｰｫｫｫｫｫ!!ﾌｫｰｯﾊｯｱﾊﾌｯﾊｯﾊｯﾊｯﾊ!!ﾄﾞﾝﾄﾞﾝﾊﾟﾁﾊﾟﾁﾊﾟﾁﾊﾟﾁﾊﾟﾁｫﾌｧｯﾌｫｯﾊｯﾊｯ!!ﾊﾋﾋﾍﾊﾌﾊｧﾄﾞﾝﾄﾞﾝﾄﾞﾝﾄﾞﾝ!!')
+    @bot.command()
+async def 螺旋の念(ctx):
+    await ctx.send('ｵｰｯ↓ﾎｯﾎｯﾎｯﾌﾎｯﾎﾌﾎﾌﾎﾌ↑ﾎﾊﾎｯﾊｯﾎｯﾎｯﾊｯﾊ(ﾄﾞﾝﾄﾞﾝﾄﾞﾝﾄﾞﾝﾄﾞﾝ)ｳﾎｳﾌﾎｳﾎﾎ↓ﾎﾎｳﾎｳﾎﾎ(ﾄﾞﾝﾄﾞﾝﾄﾞﾝ)ｳｯﾎｯﾎｯﾎ(ﾊﾞﾀﾝﾊﾞﾀﾝ)ﾊｯﾊｯﾊｯﾊｯｱﾊｯｱﾊｯ(ﾊﾞﾀﾝ)ﾊｯﾊｱﾊｯｯﾊｯﾊｯｱﾊ出たwww出たっ出た出た(ﾄﾞﾝ)ｯﾊｯﾊｯﾊｯﾊｗｗｗｱｯﾊｧ出たwwwwﾓｳｯﾎｫｯﾊｯﾊｯﾊｯﾊｯﾊｯﾊwww出たwww(ﾄﾞﾝ)wwwwwwｱｯﾊｧww出たwwwwwｯﾊｧｯﾊwwwww出たwwwwww出たｯﾊｯﾊｱｯﾊｯﾊwwww出たぁww出ったぁwwwwwwﾎﾝﾄwwwﾃﾞﾀwwwwwwwwﾌｧｰｯﾊｱやったぁwwwwwwﾊｰｯﾊﾊｯﾊwww本当にもうwwwwwwwｯﾊｯﾊｯﾊﾊｯﾊww
+(ｶﾞﾀﾝ)ｱｲﾀﾞﾀﾞﾀﾞﾀﾞ今ｯ (ｶﾞﾝｯｶﾞﾝ)ｱｲｯﾀﾞ...ちょっｱｯｯ ｱﾊｱｧ痛ってぇ... ｱｧ...ﾊｧｧ')
+    @bot.command()
+async def さもさん(ctx):
+    await ctx.send('よっちゃｗ')
+    @bot.command()
+async def 決闘(ctx):
+    await ctx.send('ディメ禁止')
+    @bot.command()
+async def 卍解(ctx):
+    await ctx.send('故に侘助')
+    @bot.command()
+async def 連絡(ctx):
+    await ctx.send('@everyoneイシス出席表 明日までに記入してくださいね')
+    @bot.command()
+async def 締め切り(ctx):
+    await ctx.send('@everyoneイシス出席 締め切りだからね')
+    @bot.command()
+async def アラド戦記(ctx):
+    await ctx.send('13年だって')
+    @bot.command()
+async def 美濃加茂(ctx):
+    await ctx.send('そうかもｗ')
+    @bot.command()
+async def 山田(ctx):
+    await ctx.send('太郎ｗ')
+    @bot.command()
+async def 詠唱1(ctx):
+    await ctx.send('天光満る処に我は在り、黄泉の門開く処に汝在り、出でよ神の雷')
+    @bot.command()
+async def 詠唱2(ctx):
+    await ctx.send('消えなさい！旋律の戒めよ！死霊使い（ネクロマンサー）の名の下に具現せよ！ミスティック・ケージ！！
+力というものを思い知りなさい！')
+    @bot.command()
+async def foo(ctx):
+    await ctx.send('Hello')
+    @bot.command()
+async def foo(ctx):
+    await ctx.send('Hello')
+    @bot.command()
+async def foo(ctx):
+    await ctx.send('Hello')
+    @bot.command()
+async def foo(ctx):
+    await ctx.send('Hello')
+    @bot.command()
+async def foo(ctx):
+    await ctx.send('Hello')
+    @bot.command()
+async def foo(ctx):
+    await ctx.send('Hello')
 bot.run(token)
 
