4bcefdb456f5ccc4e79c1f6b219f1b0edfe89565
myonnahito <56863823+myonnahito@users.noreply.github.com>
2019-10-23 20:46:51 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -25,10 +25,6 @@ async def SendMessage():
 
 await client.change_presence(activity=discord.Game(name='my game'))
 
-# or, for watching:
-activity = discord.Activity(name='my activity', type=discord.ActivityType.watching)
-await client.change_presence(activity=activity)
-
 @bot.event
 async def on_command_error(ctx, error):
     await ctx.send(str(error))
