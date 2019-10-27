1c5880f916ed4e1a3a7faf23c9573ae3f0711c24
myonnahito <56863823+myonnahito@users.noreply.github.com>
2019-10-27 11:43:32 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,13 +1,4 @@
 from discord.ext import commands
-from discord.ext import tasks
-from datetime import datetime
-import os
-import traceback
-bot = commands.Bot(command_prefix='___')
-token = os.environ['DISCORD_BOT_TOKEN']
-CHANNEL_ID = 633125958848610308 #チャンネルID
-
-
 @bot.command()
 async def echo(ctx, *, message: str):
     await ctx.send(message)
@@ -24,62 +15,6 @@ async def on_message(message):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-
-@bot.command()
-async def タクティパンツ(ctx):
-    await ctx.send('https://www.youtube.com/watch?v=DVOe5nPzgWw')
-
-@bot.command()
-async def 螺旋の念(ctx):
-    await ctx.send('https://www.youtube.com/watch?v=gwmNr729bYo')
-
-@bot.command()
-async def さもさん(ctx):
-    await ctx.send('よっちゃｗ')
-
-@bot.command()
-async def 決闘(ctx):
-    await ctx.send('ディメ禁止')
-
-@bot.command()
-async def 卍解(ctx):
-    await ctx.send('故に侘助')
-
-@bot.command()
-async def 連絡(ctx):
-    await ctx.send('@everyoneイシス出席表 明日までに記入してくださいね')
-
-@bot.command()
-async def 締め切り(ctx):
-    await ctx.send('@everyoneイシス出席 締め切りだからね')
-
-@bot.command()
-async def アラド戦記(ctx):
-    await ctx.send('13年だって')
-
-@bot.command()
-async def 美濃加茂(ctx):
-    await ctx.send('そうかもｗ')
-
-@bot.command()
-async def 山田(ctx):
-    await ctx.send('太郎ｗ')
-
-@bot.command()
-async def 詠唱1(ctx):
-    await ctx.send('天光満る処に我は在り、黄泉の門開く処に汝在り、出でよ神の雷')
-
-@bot.command()
-async def 詠唱2(ctx):
-    await ctx.send('消えなさい！旋律の戒めよ！死霊使い（ネクロマンサー）の名の下に具現せよ！ミスティック・ケージ！！力というものを思い知りなさい！')
-
-@bot.command()
-async def w(ctx):
-    await ctx.send('草生やすな')
-
-@bot.command()
-async def ww(ctx):
-    await ctx.send('ダブルの衝撃ｗ')
 
 bot.run(token)
 
